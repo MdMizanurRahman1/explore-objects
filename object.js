fa931f9833c3b4647754d2f5bd09ae44ff5b0a53
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-02-27 10:52:59 +0200

    object practiced

---
+++ object.js
@@ -0,0 +1,12 @@
+//create objects using object literates
+
+const player = {};
+player.name = 'abdul';
+player.divison = 'Sirajganj';
+player.age = 30;
+player.run = function () {
+    console.log('He did not make any good score')
+};
+
+console.log(player);
+player.run();
